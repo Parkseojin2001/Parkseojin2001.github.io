d79710e8ff58a67b1529d0d09037325d4dc6dca4
Parkseojin2001 <seojin0510612@gmail.com>
2025-02-04 23:47:13 +0900

    Updated

--- Gemfile
+++ Gemfile
@@ -5,3 +5,9 @@ gem 'jekyll-admin', group: :jekyll_plugins
 gem 'jekyll-sitemap'
 gem "github-pages", "=228"
 gem "jekyll", "=3.9.3"
+gem 'csv'
+gem 'webrick', '~> 1.7'
+gem 'rack', '~> 2.2'
+gem 'faraday-retry', '~> 2.0'
+gem "rack-protection", "~> 2.0"
+gem "sinatra-contrib", "~> 2.0"
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "[포스팅 예시] 이곳에 제목을 입력하세요",
+        "excerpt":"🦥 본문   본문은 여기에 …  ","categories": ["Algorithm"],
+        "tags": ["algorithm"],
+        "url": "/algorithm/sorting/",
+        "teaser": null
+      },{
+        "title": "[포스팅 예시] 이곳에 제목을 입력하세요",
+        "excerpt":"🦥 본문   본문은 여기에 …  ","categories": ["Capstone Design"],
+        "tags": ["capstone-design"],
+        "url": "/capstone-design/ai/",
+        "teaser": null
+      },{
+        "title": "[포스팅 예시] 이곳에 제목을 입력하세요",
+        "excerpt":"🦥 본문   본문은 여기에 …  ","categories": ["Data Structure"],
+        "tags": ["data-structure"],
+        "url": "/data-structure/stack/",
+        "teaser": null
+      },{
+        "title": "[포스팅 예시] 이곳에 제목을 입력하세요",
+        "excerpt":"🦥 본문   본문은 여기에 …  ","categories": ["Machine Learning"],
+        "tags": ["machine-learning"],
+        "url": "/machine-learning/SL/",
+        "teaser": null
+      },{
+        "title": "[포스팅 예시] 이곳에 제목을 입력하세요",
+        "excerpt":"🦥 본문   본문은 여기에 …  ","categories": ["Python"],
+        "tags": ["python"],
+        "url": "/python/functions/",
+        "teaser": null
+      }]
