06ac137ff3a938599b3faa9b5783ebffc04b6048
Parkseojin2001 <seojin0510612@gmail.com>
2025-02-09 00:26:40 +0900

    Updated deep-learning_AL

--- _posts/2025-02-07-data-structure-nonlinear.md
+++ _posts/2025-02-07-data-structure-nonlinear.md
@@ -12,12 +12,12 @@ permalink: /data-structure/nonlinear/
 toc: true
 toc_sticky: true
 
-date: 2025-02-04
-last_modified_at: 2025-02-04
+date: 2025-02-07
+last_modified_at: 2025-02-07
 ---
 
 # 🦥 그래프
-
+> 수학에서, 좀 더 구체적으로 그래프 이론에서 그래프란 객체의 일부 쌍(pair)들이 '연관되어' 있는 객체 집합 구조를 말한다.
 
 
 
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,10 +1,4 @@
 var store = [{
-        "title": "정렬",
-        "excerpt":"🦥 버블 정렬   🦥 삽입 정렬   🦥 합병 정렬   🦥 힙 정렬   🦥 퀵 정렬  ","categories": ["Algorithm"],
-        "tags": ["algorithm"],
-        "url": "/algorithm/sorting/",
-        "teaser": null
-      },{
         "title": "[포스팅 예시] 이곳에 제목을 입력하세요",
         "excerpt":"🦥 본문   본문은 여기에 …  ","categories": ["Capstone Design"],
         "tags": ["capstone-design"],
@@ -29,15 +23,27 @@ var store = [{
         "url": "/python/basic/",
         "teaser": null
       },{
-        "title": "비선형 자료구조",
-        "excerpt":"🦥 그래프   🦥 트리   🦥 힙   🦥 트라이   ","categories": ["Data Structure"],
-        "tags": ["data-structure"],
-        "url": "/data-structure/nonlinear/",
-        "teaser": null
-      },{
         "title": "선형 자료구조",
         "excerpt":"🦥 배열 배열(Array)은 값 또는 변수 엘리먼트의 집합으로 구성된 구조로, 하나 이상의 인덱스 또는 키로 식별된다. ADT의 실제 구현 대부분은 배열 또는 연결 리스트를 기반으로 한다. 배열은 크기를 지정하고 해당 크기만큼의 연속된 메모리 공간을 할당받는 작업을 수행하는 자료형을 말한다. 배열은 큐 구현에 사용되는 자료형이다. int arr[5] = {4, 7, 29,...","categories": ["Data Structure"],
         "tags": ["data-structure"],
         "url": "/data-structure/linear/",
+        "teaser": null
+      },{
+        "title": "정렬",
+        "excerpt":"   정렬 알고리즘은 목록의 요소를 특정 순서대로 넣는 알고리즘이다. 대개 숫자식 순서(Numerical Order)와 사전식 순서(Lexicographical Order)로 정렬한다.    🦥 버블 정렬   🦥 삽입 정렬   🦥 합병 정렬   🦥 힙 정렬   🦥 퀵 정렬  ","categories": ["Algorithm"],
+        "tags": ["algorithm"],
+        "url": "/algorithm/sorting/",
+        "teaser": null
+      },{
+        "title": "비선형 자료구조",
+        "excerpt":"🦥 그래프     수학에서, 좀 더 구체적으로 그래프 이론에서 그래프란 객체의 일부 쌍(pair)들이 ‘연관되어’ 있는 객체 집합 구조를 말한다.    🦥 트리   🦥 힙   🦥 트라이   ","categories": ["Data Structure"],
+        "tags": ["data-structure"],
+        "url": "/data-structure/nonlinear/",
+        "teaser": null
+      },{
+        "title": "10장 케라스를 사용한 인공 신경망 소개",
+        "excerpt":"지능적인 기계를 만드는 법에 대한 영감을 얻으려면 뇌 구조를 살펴보는 것이 합리적이다. 이는 인공신경망(ANN; Artificial Neural Network)을 촉발시킨 근원이다. 인공신경망은 뇌에 있는 생물학적 뉴런의 네트워크에서 영감을 받은 머신러닝 모델이다. 하지만 최근 인공 신경망은 생물학적 뉴런에서 점점 멀어지고 있으며 이러한 특징을 반영하기 위해 뉴런을 대신해 유닛(unit)이라고 부른다. 10.1 생물학적 뉴런에서 인공...","categories": ["Deep Learning"],
+        "tags": ["deep-learning"],
+        "url": "/deep-learning/AL/",
         "teaser": null
       }]
