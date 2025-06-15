cebcd5a2f0c8a7206f2ddf8cb6fc607f3489a3cd
Parkseojin2001 <seojin0510612@gmail.com>
2025-06-15 18:44:04 +0900

    Change Theme

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1749912219',resources: [
+    cacheName: 'chirpy-1749980614',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
