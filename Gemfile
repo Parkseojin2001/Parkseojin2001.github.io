b7069ef081aabe4aa9563e3d9767448c6e53278d
Parkseojin2001 <seojin0510612@gmail.com>
2025-06-15 19:08:00 +0900

    Updated

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@
 source "https://rubygems.org"
 
 gem "jekyll-theme-chirpy", "~> 7.3"
-gem "github-pages", group: :jekyll_plugins
+gem "jekyll", "~> 4.3"
 
 gem "html-proofer", "~> 5.0", group: :test
 
--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1749981815',resources: [
+    cacheName: 'chirpy-1749982075',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
