52abb99bf21df3a2303bb1c42d4976e0fce44503
Parkseojin2001 <seojin0510612@gmail.com>
2025-06-16 19:54:13 +0900

    Updated avatar

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1750069715',resources: [
+    cacheName: 'chirpy-1750071204',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
