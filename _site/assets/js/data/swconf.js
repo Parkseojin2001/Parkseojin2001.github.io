ae98644d9f7e18d667f9c330e960d726b524378c
Parkseojin2001 <seojin0510612@gmail.com>
2025-07-06 17:09:02 +0900

    Updated precourse1-2.md

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1751547931',resources: [
+    cacheName: 'chirpy-1751789311',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
