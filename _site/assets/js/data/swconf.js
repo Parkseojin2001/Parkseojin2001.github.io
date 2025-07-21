541c1dc068dccee84e2d6c75266a1e141501e09a
Parkseojin2001 <seojin0510612@gmail.com>
2025-07-21 23:35:44 +0900

    Updated precourse1-numpy.md

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1753031265',resources: [
+    cacheName: 'chirpy-1753108517',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
