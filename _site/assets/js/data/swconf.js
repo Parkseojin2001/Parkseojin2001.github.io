8d4c33f3529f7a13da21e31d40302a1e8fee27a1
Parkseojin2001 <seojin0510612@gmail.com>
2025-07-30 23:14:54 +0900

    Update textcnn code

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1753865455',resources: [
+    cacheName: 'chirpy-1753884874',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
