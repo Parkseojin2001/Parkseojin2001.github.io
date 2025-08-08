643f97d0eccad3f0c439b6fd98acce4b10428e00
Parkseojin2001 <seojin0510612@gmail.com>
2025-08-08 20:08:08 +0900

    Updated precourse2-generative-model.md

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1754651199',resources: [
+    cacheName: 'chirpy-1754651249',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
