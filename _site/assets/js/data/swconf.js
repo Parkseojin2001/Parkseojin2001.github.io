b466a4a2304d64103ae0c8471d376080cdd8f01a
Parkseojin2001 <seojin0510612@gmail.com>
2025-10-21 23:45:38 +0900

    Updated

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1761046740',resources: [
+    cacheName: 'chirpy-1761057921',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
