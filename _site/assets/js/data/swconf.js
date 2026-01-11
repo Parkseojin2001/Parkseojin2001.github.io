282f6903b1ad045ab4cce6fc002123b27eeedd9b
Parkseojin2001 <seojin0510612@gmail.com>
2026-01-11 21:32:40 +0900

    feat: lock naver boostcamp ai tech posts

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1768134427',resources: [
+    cacheName: 'chirpy-1768134635',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
